849fe7ca96af0c8c64999952d7183713163cb0d9
luizfm <luiz.ado@hotmail.com>
2023-01-13 22:05:35 -0300

    setup: Project initial setup

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  &:focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.colors.blue};
+  }
+
+  body {
+    background-color: ${(props) => props.theme.colors.background};
+    -webkit-font-smoothing: antialiased;
+    color: ${(props) => props.theme.colors.text};
+  }
+
+  body, input, button, textarea {
+    font: 400 1rem ${(props) => props.theme.typography.family.default};
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,33 @@
+export const defaultTheme = {
+  colors: {
+    blue: '#3294f8',
+    title: '#e7edf4',
+    subtitle: '#c4d4e3',
+    text: '#afc2d4',
+    span: '#7b96b2',
+    label: '#3a536b',
+    border: '#1c2f41',
+    post: '#112131',
+    profile: '#0b1b2b',
+    background: '#071422',
+    input: '#040f1a',
+  },
+  typography: {
+    size: {
+      xSmall: '12px',
+      small: '14px',
+      medium: '16px',
+      large: '18px',
+      xLarge: '20px',
+      xxLarge: '24px',
+    },
+    lineHeight: 1.6,
+    weight: {
+      regular: 400,
+      bold: 700,
+    },
+    family: {
+      default: `Nunito, sans-serif`,
+    },
+  },
+} as const
